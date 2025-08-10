1fe6f401a40bcd0476e7c0d46292f9fc06f2416a
Akitlove <416613578@qq.com>
2025-08-10 10:06:38 +0800

    sm8250-common: Add android.hardware.drm@1.4 and libcrypto_shim

--- kona.mk
+++ kona.mk
@@ -193,7 +193,9 @@ $(call inherit-product, hardware/dolby/dolby.mk)
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm-service.clearkey \
+    libcrypto_shim.vendor
 
 # fastbootd
 PRODUCT_PACKAGES += \
