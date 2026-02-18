87b04c830a1e7417d7846c51f9b452051d99a609
Yosaki-sonako <sonakoli122@gmail.com>
2026-02-19 00:53:00 +0800

    Revert "sm8250-common: parts: GameBar: Introduce Ram temp and Ram clock speed overlay"
    This reverts commit 7f17d806776e419a15331812f3bd5a95ceff8be4.

--- parts/src/org/lineageos/settings/gamebar/GameBarFragment.java
+++ parts/src/org/lineageos/settings/gamebar/GameBarFragment.java
@@ -66,8 +66,6 @@ public class GameBarFragment extends PreferenceFragmentCompat {
     private ListPreference mPositionPref;
     private ListPreference mSplitModePref;
     private ListPreference mOverlayFormatPref;
-    private SwitchPreferenceCompat mRamSpeedSwitch;
-    private SwitchPreferenceCompat mRamTempSwitch;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -87,8 +85,6 @@ public class GameBarFragment extends PreferenceFragmentCompat {
         mGpuUsageSwitch     = findPreference("game_bar_gpu_usage_enable");
         mGpuClockSwitch     = findPreference("game_bar_gpu_clock_enable");
         mGpuTempSwitch      = findPreference("game_bar_gpu_temp_enable");
-        mRamSpeedSwitch     = findPreference("game_bar_ram_speed_enable");
-        mRamTempSwitch      = findPreference("game_bar_ram_temp_enable");
 
         mCaptureStartPref   = findPreference("game_bar_capture_start");
         mCaptureStopPref    = findPreference("game_bar_capture_stop");
@@ -217,18 +213,6 @@ public class GameBarFragment extends PreferenceFragmentCompat {
         if (mGpuTempSwitch != null) {
             mGpuTempSwitch.setOnPreferenceChangeListener((pref, newValue) -> {
                 mGameBar.setShowGpuTemp((boolean) newValue);
-                return true;
-            });
-        }
-        if (mRamSpeedSwitch != null) {
-            mRamSpeedSwitch.setOnPreferenceChangeListener((pref, newValue) -> {
-                mGameBar.setShowRamSpeed((boolean) newValue);
-                return true;
-            });
-        }
-        if (mRamTempSwitch != null) {
-            mRamTempSwitch.setOnPreferenceChangeListener((pref, newValue) -> {
-                mGameBar.setShowRamTemp((boolean) newValue);
                 return true;
             });
         }
